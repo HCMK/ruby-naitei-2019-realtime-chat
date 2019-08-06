7bc7fc931c2b30e3b21e3e5c74e4db32f91a9e48
thanh binh <le.thanh.binh@sun-asterisk.com>
2019-08-06 16:18:23 +0700

    accept invite and destroy this invite

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
-  resources :rooms, only: :create
+  resources :users, only: :show
+  post "/users/search", to: "users#search"
+  resources :rooms, only: [:create, :show]
+  resources :invites, only: :create
+  resources :join_rooms, only: :create
   root "static_pages#home"
 end
